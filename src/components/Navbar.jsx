8313cf62262edf2b29a81b6e50aa77b2b3cd62f7
Kunal Rathore <135549137+koderrathore@users.noreply.github.com>
2026-02-15 22:55:02 +0530

    fix frontend more

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
           {menuOpen ? <X /> : <TextAlignJustify />}
         </div>
         <div
-          className={` items-center text-xl flex justify-around py-4 px-2 absolute top-16 w-full h-12 overflow-hidden transition-all ease-in-out duration-500 left-0 bg-[rgb(179,153,179)] ${menuOpen?"block":"hidden"}`}
+          className={` items-center text-xl flex justify-around py-4 px-2 absolute top-16 w-full h-12 overflow-hidden transition-all ease-in-out duration-500 left-0 bg-[rgb(179,153,179)] ${menuOpen?"block z-50":"hidden"}`}
         >
           <Link
             to={"/"}
