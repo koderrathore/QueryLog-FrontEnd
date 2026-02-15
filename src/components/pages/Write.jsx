9ad6a0e910e6b825b1507f1f68753e9df1bb9d6b
Kunal Rathore <135549137+koderrathore@users.noreply.github.com>
2026-02-15 15:03:11 +0530

    Refactor Write component to include authentication

--- src/components/pages/Write.jsx
+++ src/components/pages/Write.jsx
@@ -4,7 +4,14 @@ import UploadImage from "../UploadImage";
 import { toast } from "react-toastify";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
-import { useAuth } from "@clerk/clerk-react";
+import {
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+  SignUp,
+  useAuth,
+  useUser,
+} from "@clerk/clerk-react";
 import { useNavigate } from "react-router-dom";
 
 const Write = () => {
@@ -91,89 +98,102 @@ const Write = () => {
     },
   });
 
+  const { isSignedIn } = useUser();
+  console.log(isSignedIn);
+
   return (
-    <div className="flex flex-col my-6 ">
-      <span className="md:text-xl font-thin">Create a New Post</span>
-      <form
-        onSubmit={(e) => handleSubmit(e)}
-        className="flex flex-col gap-6 mt-4 min-h-[600px]"
-      >
-        <UploadImage
-          coverImageFile={coverImageFile}
-          setCoverImageFile={setCoverImageFile}
-          contentImageFile={contentImageFile}
-          setContentImageFile={setContentImageFile}
-          coverImage={coverImage}
-          setCoverImage={setCoverImage}
-          contentImage={contentImage}
-          setContentImage={setContentImage}
-        />
-        <input
-          required
-          value={title}
-          onChange={(e) => setTitle(e?.target?.value)}
-          type="text"
-          name=""
-          id=""
-          placeholder="My Awesome Story "
-          className="bg-transparent outline-none text-4xl"
-        />
-        <div className="flex items-center gap-2">
-          <label htmlFor="category">Choose category</label>
-          <select
-            value={category}
-            onChange={(e) => setCategory(e?.target?.value)}
-            name="category"
-            id="category"
-            className="w-fit rounded-md "
+    <>
+      <SignedIn>
+        <div className="flex flex-col my-6 ">
+          <span className="md:text-xl font-thin">Create a New Post</span>
+          <form
+            onSubmit={(e) => handleSubmit(e)}
+            className="flex flex-col gap-6 mt-4 min-h-[600px]"
           >
-            <option value="" disabled selected hidden>
-              Category
-            </option>
-            <option value="general">General</option>
-            <option value="development">Development</option>
-            <option value="databases">Databases</option>
-            <option value="marketing">Marketing</option>
-            <option value="tvShows/movies">TV Shows/Movies</option>
-            <option value="search-engines">Search Engines</option>
-            <option value="web-design">Web Design</option>
-          </select>
-        </div>
-        <div className="lg:flex gap-4 md:flex-1">
-          <div className="flex flex-col lg:w-1/4 ">
-            <textarea
+            <UploadImage
+              coverImageFile={coverImageFile}
+              setCoverImageFile={setCoverImageFile}
+              contentImageFile={contentImageFile}
+              setContentImageFile={setContentImageFile}
+              coverImage={coverImage}
+              setCoverImage={setCoverImage}
+              contentImage={contentImage}
+              setContentImage={setContentImage}
+            />
+            <input
               required
-              value={description}
-              onChange={(e) => setDescription(e?.target?.value)}
+              value={title}
+              onChange={(e) => setTitle(e?.target?.value)}
               type="text"
-              placeholder="Add a short description"
-              className="lg:min-h-1/2 w-full md:min-h-12 md:max-h-12 lg:min-h-44 min-h-12 rounded-md"
-            ></textarea>
+              name=""
+              id=""
+              placeholder="My Awesome Story "
+              className="bg-transparent outline-none text-4xl"
+            />
+            <div className="flex items-center gap-2">
+              <label htmlFor="category">Choose category</label>
+              <select
+                value={category}
+                onChange={(e) => setCategory(e?.target?.value)}
+                name="category"
+                id="category"
+                className="w-fit rounded-md "
+              >
+                <option value="" disabled selected hidden>
+                  Category
+                </option>
+                <option value="general">General</option>
+                <option value="development">Development</option>
+                <option value="databases">Databases</option>
+                <option value="marketing">Marketing</option>
+                <option value="tvShows/movies">TV Shows/Movies</option>
+                <option value="search-engines">Search Engines</option>
+                <option value="web-design">Web Design</option>
+              </select>
+            </div>
+            <div className="lg:flex gap-4 md:flex-1">
+              <div className="flex flex-col lg:w-1/4 ">
+                <textarea
+                  required
+                  value={description}
+                  onChange={(e) => setDescription(e?.target?.value)}
+                  type="text"
+                  placeholder="Add a short description"
+                  className="lg:min-h-1/2 w-full md:min-h-12 md:max-h-12 lg:min-h-44 min-h-12 rounded-md"
+                ></textarea>
+                <button
+                  type="submit"
+                  className="w-full h-full hidden lg:block text-white bg-blue-600 p-2 rounded-md hover:bg-blue-800"
+                >
+                  Publish
+                </button>
+              </div>
+              <textarea
+                required
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                className="w-full bg-white rounded-md mt-2 lg:mt-0 flex-1 min-h-72 md:min-h-56 md:max-h-56"
+                type="text"
+                placeholder="Add your content"
+              ></textarea>
+            </div>
             <button
+              disabled={mutation.isPending}
               type="submit"
-              className="w-full h-full hidden lg:block text-white bg-blue-600 p-2 rounded-md hover:bg-blue-800"
+              className="lg:hidden text-white bg-blue-600 p-2 w-fit rounded-md hover:bg-blue-800 -mt-4"
             >
-              Publish
+              {mutation.isPending ? "Publishing..." : "Publish"}
             </button>
-          </div>
-          <textarea
-            required
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            className="w-full bg-white rounded-md mt-2 lg:mt-0 flex-1 min-h-72 md:min-h-56 md:max-h-56"
-            type="text"
-            placeholder="Add your content"
-          ></textarea>
+          </form>
         </div>
-        <button
-          disabled={mutation.isPending}
-          type="submit"
-          className="lg:hidden text-white bg-blue-600 p-2 w-fit rounded-md hover:bg-blue-800 -mt-4"
-        >
-          {mutation.isPending ? "Publishing..." : "Publish"}
-        </button>
-      </form>
-    </div>
+      </SignedIn>
+      <div className="flex justify-center items-center">
+
+      <SignedOut>
+        <SignUp signInUrl="/login" />
+      </SignedOut>
+      </div>
+    </>
   );
 };
 
